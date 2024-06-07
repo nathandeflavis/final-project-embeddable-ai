38397b4d889cbad6954dfac65c62d1baa73a4167
nathandeflavis <92447278+nathandeflavis@users.noreply.github.com>
2024-06-07 12:58:25 -0400

    Task 7

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -1,8 +1,7 @@
 import json
 import requests
 
-#Task 2:
-#3. Write the function to run emotion detection using the appropriate Emotion Detection function.
+#Task 2: 3. Write the function to run emotion detection using the appropriate Emotion Detection function.
 #Name this function emotion_detector.
 #Assume that that text to be analyzed is passed to the function as an argument and is stored in the variable text_to_analyze.
 def emotion_detector(text_to_analyze):
@@ -10,17 +9,28 @@ def emotion_detector(text_to_analyze):
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     myobj = { "raw_document": { "text": text_to_analyze } }
     response = requests.post(url, json = myobj, headers = headers)
-    #Task 3:
-    #1. Convert the response text into a dictionary using the json library functions.
+    #Task 3: 1. Convert the response text into a dictionary using the json library functions.
     formatted_response = json.loads(response.text)
-    #2. Extract the required set of emotions, including anger, disgust, fear, joy and sadness, along with their scores.
-    emotion_predictions = (formatted_response["emotionPredictions"])[0]
-    emotion_scores = emotion_predictions["emotion"]
-    #3. Write the code logic to find the dominant emotion, which is the emotion with the highest score.
-    dominant_emotion = get_dominant_emotion(emotion_scores)
-    #4. modify the emotion_predictor [sic] function to return the specified output format
-    formatted_output = dict(emotion_scores)
-    formatted_output['dominant_emotion'] = dominant_emotion
+    #Task 7: 1. Access the status_code attribute of the server response to correctly display the system response for blank entries.
+    #For status_code = 400, make the function return the same dictionary, but with values for all keys being None.
+    formatted_output = {}
+
+    if response.status_code == 400:
+        keys = formatted_response.keys()
+
+        for key in keys:
+            formatted_output[key] = None
+
+        formatted_output['dominant_emotion'] = None
+    else:
+        #Task 3: 2. Extract the required set of emotions, including anger, disgust, fear, joy and sadness, along with their scores.
+        emotion_predictions = (formatted_response['emotionPredictions'])[0]
+        emotion_scores = emotion_predictions['emotion']
+        #Task 3: 3. Write the code logic to find the dominant emotion, which is the emotion with the highest score.
+        dominant_emotion = get_dominant_emotion(emotion_scores)
+        #Task 3: 4. modify the emotion_predictor [sic] function to return the specified output format
+        formatted_output = dict(emotion_scores)
+        formatted_output['dominant_emotion'] = dominant_emotion
     return formatted_output
 
 def get_dominant_emotion(emotion_scores):
--- server.py
+++ server.py
@@ -1,36 +1,41 @@
-#Task 6
-#1. Create the server.py file from scratch.
+#Task 6: 1. Create the server.py file from scratch.
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
 app = Flask("Emotion Detector")
 
-#2. Make sure that the Flask decorator for the application calling function is \emotionDetector.
+#Task 6: 2. Make sure that the Flask decorator for the application calling function is \emotionDetector.
 @app.route("/emotionDetector")
 def em_detector():
     text_to_analyze = request.args.get('textToAnalyze')
     response = emotion_detector(text_to_analyze)
-    #3. Display the output in the specified format.
-    anger = response["anger"]
-    disgust = response["disgust"]
-    fear = response["fear"]
-    joy = response["joy"]
-    sadness = response["sadness"]
+    #Task 6: 3. Display the output in the specified format.
+    #Task 7: 3. Incorporate error handling when the dominant_emotion is None.
+    #In this scenario, the response should display a message Invalid text! Please try again!.
     dominant_emotion = response["dominant_emotion"]
 
-    output = "For the given statement, the system response is "
-    output += f"'anger': {anger}, "
-    output += f"'disgust': {disgust}, "
-    output += f"'fear': {fear}, "
-    output += f"'joy': {joy} and "
-    output += f"'sadness': {sadness}. "
-    output += f"The dominant emotion is {dominant_emotion}."
+    if dominant_emotion is None:
+        output = "Invalid text! Please try again!"
+    else:
+        anger = response["anger"]
+        disgust = response["disgust"]
+        fear = response["fear"]
+        joy = response["joy"]
+        sadness = response["sadness"]
+
+        output = "For the given statement, the system response is "
+        output += f"'anger': {anger}, "
+        output += f"'disgust': {disgust}, "
+        output += f"'fear': {fear}, "
+        output += f"'joy': {joy} and "
+        output += f"'sadness': {sadness}. "
+        output += f"The dominant emotion is {dominant_emotion}."
     return output
 
 @app.route("/")
 def render_index_page():
     return render_template('index.html')
 
-#4. Application needs to be deployed on localhost:5000
+#Task 6: 4. Application needs to be deployed on localhost:5000
 if __name__ == "__main__":
     app.run(host = "0.0.0.0", port = 5000)
