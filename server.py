0d07fb61f99aa237fa763fb19206231d263ac1fd
nathandeflavis <92447278+nathandeflavis@users.noreply.github.com>
2024-06-07 16:08:58 -0400

    Task 8

--- server.py
+++ server.py
@@ -1,12 +1,20 @@
+"""
+Functions:
+• em_detector: Analyse client request text for emotions
+• render_index_page: Render the homepage template
+"""
 #Task 6: 1. Create the server.py file from scratch.
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
 app = Flask("Emotion Detector")
 
-#Task 6: 2. Make sure that the Flask decorator for the application calling function is \emotionDetector.
+#Task 6:
+#2. Make sure that the Flask decorator for the application calling function is \emotionDetector.
 @app.route("/emotionDetector")
 def em_detector():
+    """Analyse client request text for emotions
+    and return a string including emotion scores and the dominant emotion."""
     text_to_analyze = request.args.get('textToAnalyze')
     response = emotion_detector(text_to_analyze)
     #Task 6: 3. Display the output in the specified format.
@@ -34,6 +42,7 @@ def em_detector():
 
 @app.route("/")
 def render_index_page():
+    """Render the homepage template and return a string which is its HTML."""
     return render_template('index.html')
 
 #Task 6: 4. Application needs to be deployed on localhost:5000
